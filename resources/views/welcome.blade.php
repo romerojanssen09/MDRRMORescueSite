ed563f53f5d2d1b4ce9794d8db8e7b088eb952f2
LikedJans <User@DESKTOP-0PF9GJ5>
2026-02-17 19:52:38 +0800

    update push part 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MDRRMO Rescue System</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @production
+        <link rel="stylesheet" href="{{ asset('build/assets/app-BEI4OL5h.css') }}">
+        <script src="{{ asset('build/assets/app-CKl8NZMC.js') }}" defer></script>
+    @else
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @endproduction
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
         /* Grid Background Pattern */
